bc5745bdd7b19504317f2244681d2935a0ed7ab3
SaiRamyaKamali <srkamali1326@gmail.com>
2020-06-17 19:02:18 +0530

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, {Component} from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render() {
     return (
       <div>
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,64 +1,77 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
 
-class DishDetail extends Component {
+
  
   
-  renderDish(dish) {
-    if (dish) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} />
+function RenderDish({dish}) {
+  if(dish)
+  {
+    return (
+      <Card>
+         <CardImg width="100%" src={dish.image} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
           </CardBody>
-        </Card>
-      );
-    }
+        </Card> 
+    )
+
   }
-  renderComments(dish) {
-    console.log(dish);
-    if (dish) {
-      return (
-        <div>
-          <h4>Comments</h4>
-          <ul className="list-group">
-            {dish.comments.map((x) => {
-              return (
-                <li key={x.id} className="list-group mt-3">
-                  <p>{x.comment}</p>
-                  <p>
-                    {"-- " +
-                      x.author +
-                      " , " +
-                      new Intl.DateTimeFormat("en-US", {
-                        year: "numeric",
-                        month: "short",
-                        day: "2-digit",
-                      }).format(new Date(Date.parse(x.date)))}
-                  </p>
-                </li>
-              );
-            })}
-          </ul>
+
+  else
+  {
+    return null;
+  }
+  
+}
+function RenderComments({dish}) {
+ if(dish){
+  return (
+    <div>
+      <h4>Comments</h4>
+      <ul className="list-group">
+        {dish.comments.map((x) => {
+          return (
+            <li key={x.id} className="list-group mt-3">
+              <p>{x.comment}</p>
+              <p>
+                {"-- " +
+                  x.author +
+                  " , " +
+                  new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(x.date)))}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+
+ } 
+
+ else
+ {
+   return null;
+ }
+
+}
+const  DishDetail = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish} />
         </div>
-      );
-    }
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.dish)}
-          </div>
+        <div className="col-12 col-md-5 m-1">
+          <RenderComments dish={props.dish}/>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default DishDetail;
